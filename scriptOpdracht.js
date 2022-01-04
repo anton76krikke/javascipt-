c4111c301b897a58bbce0c3195b2bc3130e2aad8
anton76krikke <73478659+anton76krikke@users.noreply.github.com>
2022-01-04 23:02:05 +0100

    Add files via upload

---
+++ scriptOpdracht.js
@@ -0,0 +1,9 @@
+console.log("Hello winc acamamy studens")
+
+/*this is a large 
+sum
+vermenigvuldiging = 1230941 * 1823794
+console.log(vermenigvuldiging)
+
+delen = 637263 / 54
+console.log(delen) */
